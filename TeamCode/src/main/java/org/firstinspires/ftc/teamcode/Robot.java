6f1d3d11d3548f100d31036add5bfe4c01928562
theo <theo@seelye.net>
2021-03-24 19:21:52 -0700

    moved code from Eli branch to master

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -0,0 +1,112 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+public class Robot {
+
+    // Robot variables and objects
+    private double leftFrontPower = 0;
+    private double rightFrontPower = 0;
+    private double leftRearPower = 0;
+    private double rightRearPower = 0;
+    public double speed = 1;
+    public double config = 0;
+
+    HardwareMap hardwareMap; // TODO : get rid of this
+
+    public DcMotor leftFrontDrive;
+    public DcMotor rightFrontDrive;
+    public DcMotor leftRearDrive;
+    public DcMotor rightRearDrive;
+
+    public ElapsedTime elapsedTime;
+    public Gyro gyro;
+    public Telemetry telemetry;
+
+    // Creates a robot object with methods that we can use in both Auto and TeleOp
+    public Robot(HardwareMap hardwareMap, Telemetry telemetry) {
+        this.hardwareMap = hardwareMap;// TODO : get rid of this
+
+        // These are the names to use in the phone config (in quotes below)
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "leftFrontDrive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFrontDrive");
+        leftRearDrive = hardwareMap.get(DcMotor.class, "leftRearDrive");
+        rightRearDrive = hardwareMap.get(DcMotor.class, "rightRearDrive");
+
+        // Defines the forward direction for each of our motors/servos
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftRearDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightRearDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        // Initializes some other useful tools for our robot (the gyroscope, the timer, etc.)
+        gyro = new Gyro(hardwareMap);
+        gyro.resetAngle();
+        elapsedTime = new ElapsedTime();
+        elapsedTime.reset();
+        this.telemetry = telemetry;
+    }
+
+    public void stopDrive() {
+        leftFrontPower = 0;
+        rightFrontPower = 0;
+        leftRearPower = 0;
+        leftRearPower = 0;
+        leftFrontDrive.setPower(leftFrontPower);
+        rightFrontDrive.setPower(rightFrontPower);
+        leftRearDrive.setPower(leftRearPower);
+        rightRearDrive.setPower(rightRearPower);
+    }
+
+    // Calculates powers for mecanum wheel drive
+    public void calculateDrivePowers(double x, double y, double rotation) {
+        double r = Math.hypot(x, y);
+        double robotAngle = Math.atan2(y, x) - Math.PI / 4;
+        leftFrontPower = Range.clip(r * Math.cos(robotAngle) + rotation, -1.0, 1.0) * speed;
+        rightFrontPower = Range.clip(r * Math.sin(robotAngle) - rotation, -1.0, 1.0) * speed;
+        leftRearPower = Range.clip(r * Math.sin(robotAngle) + rotation, -1.0, 1.0) * speed;
+        rightRearPower = Range.clip(r * Math.cos(robotAngle) - rotation, -1.0, 1.0) * speed;
+    }
+
+    // Sends desired power to drive motors
+    public void sendDrivePowers() {
+        leftFrontDrive.setPower(leftFrontPower);
+        rightFrontDrive.setPower(rightFrontPower);
+        leftRearDrive.setPower(leftRearPower);
+        rightRearDrive.setPower(rightRearPower);
+    }
+
+    // Updates the powers being sent to the drive motors
+    public void updateDrive() {
+        //Displays motor powers on the phone
+        telemetry.addData("angle", "" + gyro.getAngle());
+        telemetry.addData("config: ", "" + config);
+        telemetry.update();
+        sendDrivePowers();
+    }
+    // Makes the robot chase the wobble goal (if called repeatedly); TO DO: NEEDS GYRO IMPLEMENTATION
+    //TEsting
+    // Toggles the drive speed between 50% and normal
+    public void toggleSpeed() {
+        speed = (speed == 1 ? 0.5 : 1);
+    }
+
+    // Resets the timer
+    public void resetElapsedTime() {
+        elapsedTime.reset();
+    }
+
+    // Returns how many seconds have passed since the timer was last reset
+    public double getElapsedTimeSeconds() {
+        return elapsedTime.seconds();
+    }
+
+    // Makes the robot wait (i.e. do nothing) for a specified number of seconds
+    public void wait(double seconds) {
+        double start = getElapsedTimeSeconds();
+        while (getElapsedTimeSeconds() - start < seconds) {}
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele1.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Controller;
+
+@TeleOp(name="Tele1", group="Iterative Opmode")
+public class Tele1 extends OpMode
+{
+    //Declare OpMode members
+    Robot robot;
+    Controller controller1;
+    //Controller controller2;
+
+    //Code to run ONCE when the driver hits INIT
+    @Override
+    public void init() {
+        robot = new Robot(hardwareMap, telemetry);
+        controller1 = new Controller(gamepad1);
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+    }
+
+    //Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+    @Override
+    public void init_loop() {}
+
+    //Code to run ONCE when the driver hits PLAY
+    @Override
+    public void start() { robot.resetElapsedTime(); }
+
+    //Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+    @Override
+    public void loop() {
+        //Registers controller input
+        controller1.update();
+
+        //Press "x" to toggle speed between 100% and 30%
+        if (controller1.x.equals("pressing")) {
+            robot.toggleSpeed();
+        }
+
+        //Mecanum wheel drive
+        robot.calculateDrivePowers(
+                controller1.left_stick_x,
+                controller1.left_stick_y,
+                controller1.right_stick_x
+        );
+        robot.updateDrive();
+
+
+    }
+
+    //Code to run ONCE after the driver hits STOP
+    @Override
+    public void stop(){}
+}
