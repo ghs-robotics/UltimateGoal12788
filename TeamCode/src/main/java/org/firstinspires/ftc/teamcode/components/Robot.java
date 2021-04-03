04afc3c9daf1418c142173f93556f856b823df09
Benjamin <benden3697@gmail.com>
2021-04-03 12:42:49 -0700

    Massive commit with the first draft of my auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/Robot.java
@@ -3,6 +3,7 @@ import android.graphics.Camera;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -16,12 +17,15 @@ public class Robot {
     private double rightFrontPower = 0;
     private double leftRearPower = 0;
     private double rightRearPower = 0;
+    private double intakePower = 0;
+    public double armAngle = 0.45; // Up position
+    public double grabAngle = 0.15; // Closed position
     public double speed = 1;
-    public double config = 0;
+    public int config = 0; //Number of rings. Updated with identifyRingConfig
     public String currentTargetObject = "ring";
     public int[] objectVals = new int[]{0,0,0,0}; //X, Y, Width, Height
     public int[] targetVals = new int[]{0,0,0,0}; //X, Y, Width, Height
-    public double targetAngle = 0; //Angle of line between the target object and the robot
+    public int targetAngle = 0; //Angle of line between the target object and the robot
     //targetAngle is not relative to the robot's orientation!
 
     public CameraManager cameraManager;
@@ -30,6 +34,10 @@ public class Robot {
     public DcMotor rightFrontDrive;
     public DcMotor leftRearDrive;
     public DcMotor rightRearDrive;
+    public DcMotor intakeMotor;
+    public Servo armServo;
+    public Servo grabServo;
+    public Servo shooterServo;
 
     public ElapsedTime elapsedTime;
     public Gyro gyro;
@@ -45,6 +53,10 @@ public class Robot {
         rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFrontDrive");
         leftRearDrive = hardwareMap.get(DcMotor.class, "leftRearDrive");
         rightRearDrive = hardwareMap.get(DcMotor.class, "rightRearDrive");
+        intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
+        armServo = hardwareMap.get(Servo.class, "armServo");
+        grabServo = hardwareMap.get(Servo.class, "grabServo");
+        shooterServo = hardwareMap.get(Servo.class, "shooterServo");
 
         // Defines the forward direction for each of our motors/servos
         leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
@@ -69,10 +81,10 @@ public class Robot {
     }
 
     // Calculates powers for mecanum wheel drive
-    public void calculateDrivePowers(double x, double y, double rotation) {
-        x *= -1.0;
-        double r = Math.hypot(x, y);
-        double robotAngle = Math.atan2(y, x) - Math.PI / 4;
+    public void calculateDrivePowers(double dX, double dY, double rotation) {
+        dX *= -1.0;
+        double r = Math.hypot(dX, dY);
+        double robotAngle = Math.atan2(dY, dX) - Math.PI / 4;
         leftFrontPower = Range.clip(r * Math.cos(robotAngle) + rotation, -1.0, 1.0) * speed;
         rightFrontPower = Range.clip(r * Math.sin(robotAngle) - rotation, -1.0, 1.0) * speed;
         leftRearPower = Range.clip(r * Math.sin(robotAngle) + rotation, -1.0, 1.0) * speed;
@@ -107,36 +119,65 @@ public class Robot {
         currentTargetObject = target;
         //xPID.resetValues(); yPID.resetValues(); We should add PID later!
         //SET LOWER AND UPPER IN CVPIPELINE
+        //Keep in mind that
         targetVals[0] = target == "ring" ? 230 : target == "wobble" ? 60 : 140;
-        targetVals[1] = target == "ring" ? 190 : target == "wobble" ? 160 : 70;
+        targetVals[1] = target == "ring" ? 190 : target == "wobble" ? 160 : 0;
+        targetVals[2] = target == "tower" ? 70 : 0;
     }
 
-    public void chaseTarget(String target) {
+    public void setTargetTo(int tX, int tY, int tW, int tH) {
+        targetVals[0] = tX; targetVals[1] = tY; targetVals[2] = tW; targetVals[3] = tH;
+    }
+
+    public void chaseTarget(String target) { //Can be used to chase any target
         double rotation = 0; //Maybe add this as parameter later
         setTargetTo(target);
         updateObjectValues();
-        calculateDrivePowers(targetVals[0], targetVals[1], rotation);
+        double dx = targetVals[0] - objectVals[0];
+        double dy = targetVals[1] - objectVals[1];
+        calculateDrivePowers(dx, dy, rotation);
         sendDrivePowers();
     }
 
-    public void moveToPos(int[] pos) {
-        double maxSeconds = 1.0; //Maybe add this as parameter later
+    //Robot lines up with target x relative to tower and with certain tower width
+    public void chaseTower() {
         setTargetTo("tower");
+        updateObjectValues();
+
+        double dx = targetVals[0] - objectVals[0];
+        double dw = targetVals[2] - objectVals[2];
+
+        if (!(objectVals[2] > 40 && objectVals[2] < 150)) { //Don't do anything if tower not found
+            telemetry.addData("Error: ", "Could not find tower goal");
+            telemetry.update();
+            return;
+        }
+        calculateDrivePowers(dx, dw, 0);
+        sendDrivePowers();
+    }
+
+    //Uses a target X relative to tower goal, and target width of tower goal
+    public void moveToPos(int posX, int posW) {
+        int maxSeconds = 1; //TODO:Adjust this later
+        currentTargetObject = "tower";
+        setTargetTo(posX, 0, posW, 0); //update target x and width
         updateObjectValues();
         double t = getElapsedTimeSeconds();
         while((Math.abs(targetVals[2] - objectVals[2]) > 8 || Math.abs(targetVals[0] - objectVals[0]) > 8)
                 && elapsedTime.seconds() - t < 5) {
-            chaseTarget("tower");
+            chaseTower();
         }
         t = getElapsedTimeSeconds();
         while ((leftRearPower != 0 || rightRearPower != 0 || leftFrontPower != 0
                 || rightFrontPower != 0) && elapsedTime.seconds() - t < maxSeconds) {
-            chaseTarget("tower");
+            chaseTower();
         }
         //stopDrive();
     }
 
-    public void rotateToPos(int angle, int maxSeconds) {
+    public void moveToPos(int[] pos) {moveToPos(pos[0], pos[1]);}
+
+    public void rotateToPos(int angle, int maxSeconds) { //Angle here is in degrees
         targetAngle = angle;
         double t = getElapsedTimeSeconds();
         while(Math.abs(targetAngle - gyro.getAngle()) > 5 && elapsedTime.seconds() - t < 5) {
@@ -149,6 +190,8 @@ public class Robot {
         }
         //stopDrive();
     }
+
+    public void rotateToPos(int angle) {rotateToPos(angle,1);}
 
     // Makes the robot line up with the tower goal and shoot three rings
     public void adjustAndShoot() {
@@ -190,6 +233,26 @@ public class Robot {
         //diffy.toggleShooter(0);
     }
 
+    public void toggleIntake() {
+        intakePower = (intakePower == 0 ? 0.6 : 0);
+        intakeMotor.setPower(intakePower);
+    }
+
+    // Turns the arm
+    public void turnArm() {
+        // Default angle is 0.5 (which is the up position)
+        armAngle = (armAngle == 0.88 ? 0.45 : 0.88);
+        armServo.setPosition(armAngle);
+    }
+
+    // Toggles the wobble gripper
+    public void toggleGrab() {
+        // Default angle is 0.15 (which means the gripper is closed)
+        grabAngle = (grabAngle == 0.48 ? 0.15 : 0.48);
+        grabServo.setPosition(grabAngle);
+    }
+
+
     //We need to figure out how to launch rings without a diffy launcher
     public void launchRing() {
         /*
@@ -198,6 +261,41 @@ public class Robot {
         wait(0.5);
         shooterAngle = 0.58; // Back position
         shooterServo.setPosition(shooterAngle); */
+    }
+
+    // Makes robot move forward and pick up wobble goal
+    public void pickUpWobbleGoal(double sec) {
+        turnArm();
+        toggleGrab();
+        calculateDrivePowers(0,-0.4,0);
+        sendDrivePowers();
+        wait(sec); //Adjust this later
+        stopDrive();
+        toggleGrab();
+        wait(0.6);
+        turnArm();
+    }
+
+    // Classifies the starter stack; TODO: NEEDS TO BE TESTED ADJUSTED
+    public int identifyRingConfig() {
+        setTargetTo("ring");
+        updateObjectValues();
+        if (!(objectVals[2] == 0)) {
+            config = (int)Math.round(1.0 * objectVals[3] / objectVals[2]);
+        }
+        return config;
+    }
+
+    // To use at the start of each OpMode that uses CV
+    public void init() {
+        resetServos();
+        cameraManager.initCamera();
+    }
+
+    // Sets servos to starting positions
+    public void resetServos() {
+        armServo.setPosition(armAngle);
+        grabServo.setPosition(grabAngle);
     }
 
     // Resets the timer
